70c03a8dea74759324cf091cf90e0f8956cd395f
Renzo <aquinren@uw.edu>
2025-07-24 23:23:57 -0700

    added GDscript comments and function

--- cool trivia maze movement concept/player.gd
+++ cool trivia maze movement concept/player.gd
@@ -1,15 +1,18 @@
-class_name Player
-extends CharacterBody2D
+# This is the Player class
+class_name Player extends CharacterBody2D
 
-
+# Fields
 @export var SPEED : float = 100.0
 @export var animation_tree : AnimationTree
-
 var input : Vector2
 var playback : AnimationNodeStateMachinePlayback
 
+# On ready function 
 func _ready():
+	# activates animations
 	playback = animation_tree["parameters/playback"]
+
+# Controls the player
 func _physics_process(_delta: float) -> void:
 	# get WASD input
 	input = Input.get_vector("left", "right", "up", "down")
@@ -19,22 +22,30 @@ func _physics_process(_delta: float) -> void:
 	move_and_slide()
 	select_animation()
 	update_animation_parameters()
-	check()
+	runActive()
 	
+# Handles animation of player
 func select_animation():
 	if velocity == Vector2.ZERO:
 		playback.travel("Idle")
 	else:
 		playback.travel("Walk")
-	
+
+# Updates animations
 func update_animation_parameters():
 	if input == Vector2.ZERO:
 		return
 	animation_tree["parameters/Idle/blend_position"] = input
 	animation_tree["parameters/Walk/blend_position"] = input
 
-func check():
+# Checks if player is holding down the run button
+func runActive():
 	if Input.is_action_pressed("run"):
 		SPEED = 200.0
 	else:
 		SPEED = 100.0
+
+# Checks if player presses accept
+func interactActive():
+	if Input.is_action_pressed("accept"):
+		pass
