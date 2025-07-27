5635d4c2df4b94fe547e9defcd84b59d10270e3d
Shiraz Arif <shirazblox@gmail.com>
2025-07-26 21:13:55 -0700

    partial implementation of maze generation/navigation system

--- cool trivia maze movement concept/scenes/Maze.gd
+++ cool trivia maze movement concept/scenes/Maze.gd
@@ -1,5 +1,14 @@
 extends Node2D
 
+# prepare assets to go (also makes debugging a little easier via inspector panel)
+@export var roomScene: PackedScene = preload("res://scenes/RoomScene.tscn")
+@export var player: NodePath
+@onready var player_node = get_node(player)
+
+# room assets (instantiated later)
+var currentRoomInstance: Node = null
+var tilemap: TileMap = null
+
 # rooms are arranged in an array which also represents their actual arrangement
 var mazeRooms: Array = []
 @export var mazeWidth: int = 7
@@ -12,17 +21,26 @@ var currentRoomY: int = 0
  
 func _ready():
 	prepareMazeArray()
-	startingRoom()
+	setStartingRoom()
+	loadRoom()
 	roomCoordsDebug()
 
+# generate tiles and store their data in an array
 func prepareMazeArray():
 	for x in range(mazeWidth):
 		var column: Array = [] # reinitialize the column array on each loop to prevent cells from pointing at the same array
 		for y in range(mazeHeight):
-			column.append(prepareRoom(x,y))
+			var thisRoom = prepareRoom(x,y)
+			print("prepared room at ", x, ",", y, " with tiles: ", thisRoom["tileData"].size())
+			column.append(thisRoom)
 		mazeRooms.append(column)
 
+# generates data for a single room
 func prepareRoom(x: int, y: int):
+	var roomInstance = roomScene.instantiate()
+	var sceneTilemap = roomInstance.get_node("Room1")
+	var tileData = getTileData(sceneTilemap)
+	
 	var room = {
 		"x": x,
 		"y": y, 
@@ -30,14 +48,35 @@ func prepareRoom(x: int, y: int):
 		"southDoor": y > 0,
 		"eastDoor": x < mazeWidth - 1,
 		"westDoor": x > 0,
-		"tilemap": "Room1"
+		"tileData": tileData
 	}
 	return room
 
-func startingRoom(): 
-	currentRoomX = mazeWidth / 2 + 1
-	currentRoomY = mazeHeight / 2 + 1
+# start the player in the middle-most room
+func setStartingRoom(): 
+	currentRoomX = int(mazeWidth / 2) + 1
+	currentRoomY = int(mazeHeight / 2) + 1
 
+func getTileData(thisTileMap: TileMap):
+	var data: Array = []
+	for cell in thisTileMap.get_used_cells(0):
+		var tileID = thisTileMap.get_cell(0, cell)
+		data.append({"position": cell, "tileID": tileID})
+	return data
+
+# load the current room
+func loadRoom():
+	if currentRoomInstance:
+		currentRoomInstance.queue_free()
+	currentRoomInstance = roomScene.instantiate()
+	add_child(currentRoomInstance)
+	tilemap = currentRoomInstance.get_node("Room1")
+	tilemap.clear()
+	var room = mazeRooms[currentRoomX][currentRoomY]
+	var data = room["tileData"]
+	for cell in data:
+		tilemap.set_cell(0, cell["position"], cell["tileID"])
+
+# show current room coordinates for debug
 func roomCoordsDebug():
-	print(currentRoomX, ",", currentRoomY)
-	
+	print("room coordinates: ", currentRoomX, ",", currentRoomY)
