cb0a4b307db15859055b6f47f2dbb6d9383ee90c
Shiraz Arif <shirazblox@gmail.com>
2025-07-26 22:55:41 -0700

    almost there i think

--- cool trivia maze/scripts/Maze.gd
+++ cool trivia maze/scripts/Maze.gd
@@ -3,7 +3,7 @@ extends Node2D
 # prepare assets to go (also makes debugging a little easier via inspector panel)
 @export var roomScene: PackedScene = preload("res://scenes/RoomScene.tscn")
 @export var player: NodePath
-@onready var player_node = get_node(player)
+@onready var playerNode = get_node(player)
 
 # room assets (instantiated later)
 var currentRoomInstance: Node = null
@@ -17,7 +17,6 @@ var mazeRooms: Array = []
 # keep track of the current room coordinates of the player
 var currentRoomX: int = 0
 var currentRoomY: int = 0
-# TODO: if able, add a timer here (only to be active in hard mode)
  
 func _ready():
 	prepareMazeArray()
@@ -60,22 +59,58 @@ func setStartingRoom():
 func getTileData(thisTileMap: TileMap):
 	var data: Array = []
 	for cell in thisTileMap.get_used_cells(0):
-		var tileID = thisTileMap.get_cell(0, cell)
+		var tileID = thisTileMap.get_cell_source_id(0, cell)
 		data.append({"position": cell, "tileID": tileID})
 	return data
 
 # load the current room
 func loadRoom():
+	# clear previously loaded room to make way for new one
 	if currentRoomInstance:
 		currentRoomInstance.queue_free()
+	# new room instance
 	currentRoomInstance = roomScene.instantiate()
 	add_child(currentRoomInstance)
 	tilemap = currentRoomInstance.get_node("Room1")
 	tilemap.clear()
+	# actually generate the room
 	var room = mazeRooms[currentRoomX][currentRoomY]
 	var data = room["tileData"]
 	for cell in data:
 		tilemap.set_cell(0, cell["position"], cell["tileID"])
+	# add door hitbox detection
+	var doors = currentRoomInstance.get_node("Doors")
+	for doorName in ["NorthDoor", "SouthDoor", "EastDoor", "WestDoor"]:
+		var door = doors.get_node(doorName)
+		door.connect("body_entered", Callable(self, "doorTouched").bind(doorName))
+
+func doorTouched(doorName: String, body: Node):
+	moveRooms(doorName)
+
+# move the player to another room when they go through a door
+func moveRooms(doorName: String):
+	var enteringFrom = ""
+	# match is literally just a switch statement
+	match doorName:
+		"NorthDoor":
+			currentRoomY -= 1
+			enteringFrom = "FromSouth"
+		"SouthDoor":
+			currentRoomY += 1
+			enteringFrom = "FromNorth"
+		"EastDoor":
+			currentRoomX += 1
+			enteringFrom = "FromWest"
+		"WestDoor":
+			currentRoomX -= 1
+			enteringFrom = "FromEast"
+	
+	loadRoom()
+	var markers = currentRoomInstance.get_node("EntryPoint")
+	var entryPoint = markers.get_node(enteringFrom)
+	playerNode.global_position = entryPoint.global_position
+	
+	roomCoordsDebug()
 
 # show current room coordinates for debug
 func roomCoordsDebug():
--- src/view/GameDisplay.java
+++
@@ -1,6 +0,0 @@
-/**
- * generates the visual elements and display window for the game
- */
-public class GameDisplay {
-
-}
