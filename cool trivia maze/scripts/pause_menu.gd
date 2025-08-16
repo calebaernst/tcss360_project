d91c8b768ec08f1111ea1fca9bdbbe35d9213bf3
Renzo <149926085+Scizzanne@users.noreply.github.com>
2025-08-16 16:22:58 -0700

    added more pause menu stuff I think

--- cool trivia maze/scripts/pause_menu.gd
+++ cool trivia maze/scripts/pause_menu.gd
@@ -7,7 +7,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	testEsc()
 
 
 func _on_save_game_button_down() -> void:
@@ -15,8 +15,24 @@ func _on_save_game_button_down() -> void:
 
 
 func _on_quit_button_down() -> void:
-	pass # Replace with function body.
+	get_tree().quit()
 
 
 func _on_resume_button_down() -> void:
-	pass # Replace with function body.
+	set_resume()
+
+# setter: resume game
+func set_resume() -> void:
+	get_tree().paused = false
+	$play_blur.play_backwards("blur_animation")
+
+# setter: pause game
+func set_pause() -> void:
+	get_tree().paused = true
+	$play_blur.play("blur_animation")
+
+func testEsc():
+	if Input.is_action_just_pressed("pause") and get_tree().paused == false:
+		set_pause()
+	elif Input.is_action_just_pressed("pause") and get_tree().paused == true:
+		set_resume()
