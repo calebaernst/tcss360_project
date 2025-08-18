8bad7e071c7dcf1d3b443f6b9caeee1277668028
Shiraz Arif <shirazblox@gmail.com>
2025-08-18 12:18:31 -0700

    ya

--- cool trivia maze/scripts/win.gd
+++ cool trivia maze/scripts/win.gd
@@ -1,5 +1,12 @@
 # Win Script
 extends Control
 
+@onready var yayy = $yayy
+@onready var fanfare = $fanfare
+
+func _ready() -> void:
+	yayy.play()
+	fanfare.play()
+
 func _on_return_button_down() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
