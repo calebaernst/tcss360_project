c5d41761a6f1facf305d8b781d08401acc522d5d
Shiraz Arif <shirazblox@gmail.com>
2025-08-18 14:31:47 -0700

    logo and defeat music

--- cool trivia maze/scripts/win.gd
+++ cool trivia maze/scripts/win.gd
@@ -1,12 +1,5 @@
 # Win Script
 extends Control
 
-@onready var yayy = $yayy
-@onready var fanfare = $fanfare
-
-func _ready() -> void:
-	yayy.play()
-	fanfare.play()
-
 func _on_return_button_down() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
